e4a362ab79e07998cea427f2672a90c10549f4f8
JulioJaiimes <113730554+JulioJaiimes@users.noreply.github.com>
2026-03-12 14:59:48 -0500

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// frontend/src/App.jsx
+import Chat from './Chat'; // Importamos nuestro nuevo componente
+
+function App() {
+  // Simplemente renderizamos el componente Chat
+  return <Chat />;
+}
+
+export default App;
